5f1541df42ae1ba13ee2e00d6fb832a63b8e1664
Kenny Pflug <kenny.pflug@live.de>
2025-05-03 20:33:31 +0200

    feat: prepare hands-on 5

--- 04-AsyncStreamingGrpc/AsyncStreamingGrpc/AsyncStreaming/NumberStreamingService.cs
+++ 04-AsyncStreamingGrpc/AsyncStreamingGrpc/AsyncStreaming/NumberStreamingService.cs
@@ -1,5 +1,6 @@
-using System;
 using System.Collections.Generic;
+using System.Runtime.CompilerServices;
+using System.Threading;
 using System.Threading.Tasks;
 using ProtoBuf.Grpc;
 
@@ -15,11 +16,42 @@ public sealed class NumberStreamingService : INumberStreamingService
         StreamNumbersRequestDto dto,
         CallContext context = default
     ) =>
-        throw new NotImplementedException();
+        dto.CancelAllStreams ?
+            StreamForAdminCallerAsync(context.CancellationToken) :
+            StreamForStandardCallerAsync(context.CancellationToken);
 
     public Task<StreamingResponseDto> StartStreaming(CallContext callContext = default)
     {
         var result = _numberGenerator.TryStartGeneratingNumbers();
         return Task.FromResult(new StreamingResponseDto { IsSuccess = result });
+    }
+    
+    // Not necessary because we register explicitly on the cancellation token
+#pragma warning disable CS8425 // Async-iterator member has one or more parameters of type 'CancellationToken' but none of them is decorated with the 'EnumeratorCancellation' attribute, so the cancellation token parameter from the generated 'IAsyncEnumerable<>.GetAsyncEnumerator' will be unconsumed
+    private async IAsyncEnumerable<StreamNumbersResponseDto> StreamForAdminCallerAsync(
+        CancellationToken cancellationToken
+    )
+    {
+        await using var registration = cancellationToken.Register(
+            state => ((NumberGenerator) state!).TryStopGeneratingNumbers(),
+            _numberGenerator
+        );
+
+        // ReSharper disable once UseCancellationTokenForIAsyncEnumerable -- already handled by the registration
+        await foreach (var number in _numberGenerator.GetAsyncEnumerable())
+        {
+            yield return new StreamNumbersResponseDto { Number = number };
+        }
+    }
+#pragma warning restore CS8425
+    
+    private async IAsyncEnumerable<StreamNumbersResponseDto> StreamForStandardCallerAsync(
+        [EnumeratorCancellation] CancellationToken cancellationToken
+    )
+    {
+        await foreach (var number in _numberGenerator.GetAsyncEnumerable().WithCancellation(cancellationToken))
+        {
+            yield return StreamNumbersResponseDto.FromNumber(number);
+        }
     }
 }
